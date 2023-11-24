ce4d94d31190d80328aa191031940a34db22cf9a
younha00 <ayeon0@office.skhu.ac.kr>
2023-11-25 03:27:08 +0900

    MockClient 사용

--- 02_unit_test_mock/test/album_test.dart
+++ 02_unit_test_mock/test/album_test.dart
@@ -1,42 +1,47 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
-import 'package:mockito/mockito.dart';
+import 'package:http/testing.dart';
 import 'package:unit_test_mock/album.dart';
 
-import 'album_test.mocks.dart';
-
-// 리스트 안에 모킹할 객체를 써줌
-@GenerateMocks([http.Client])
 void main() {
   group('fetchAlbum', () {
     test('Album이 정상적으로 반환되어야한다', () async {
-      // 가짜 클라이언트를 만들 것임(Mockito로)
-      final client = MockClient();
-      // when에는 내가 동작시킬 메서드의 결과를 가상으로 만들 수 있음
-      // client.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')); 이 부분
-      when(client
-              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
-          .thenAnswer(
-        (_) async => http.Response(
-          // 위 요청을 보냈을 때 정상일 때의 값
-          '{"userId": 1, "id": 1, "title": "quidem molestiae enim"}',
-          200,
-        ),
-      );
+      // http.testing.dart에서 제공하는 MockClient를 사용
+      // Response를 반환하는 함수 작성
+      final client = MockClient((request) async {
+        // 모킹할 내용 작성
+        if (request.url.toString() ==
+            'https://jsonplaceholder.typicode.com/albums/1') {
+          return http.Response(
+            '{"userId": 1, "id": 1, "title": "quidem molestiae enim"}',
+            200,
+          );
+        }
+        return http.Response(
+          'error',
+          404,
+        );
+      });
+
       // isA는 타입 검사, 테스트할 때 주로 사용
       expect(await fetchAlbum(client), isA<Album>());
     });
+
+    // 404인 경우
     test('404 에러인 경우에는 예외를 발생시켜야한다', () async {
-      final client = MockClient();
-      when(client
-              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
-          .thenAnswer(
-        (_) async => http.Response(
-          'Not Found',
+      final client = MockClient((request) async {
+        if (request.url.toString() ==
+            'https://jsonplaceholder.typicode.com/albums/1') {
+          return http.Response(
+            'Not Found',
+            404,
+          );
+        }
+        return http.Response(
+          'error',
           404,
-        ),
-      );
+        );
+      });
       expect(
         // 함수 자체를 써줘야지 결과를 넣는 게 아니므로 await를 쓰지 않음
         fetchAlbum(client),
